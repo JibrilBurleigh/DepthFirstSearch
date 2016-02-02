1e38289815da3b71bc7a395973eb43afe838225d
jibbgh <jibbgh@gmail.com>
2016-02-02 00:37:13 -0500

    Put the final touches on the DFS class. Moving on to Node class tommorow.

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -1,14 +1,10 @@
 import java.util.Hashtable;
-import java.util.Stack;
 
-/**
- * Created by jibbgh on 1/26/2016.
- */
+
 public class DepthFirstSearch<T>
 {
     public Node<T> root;
     public Node<T> goal;
-    public Node<T> child;
     public Hashtable<Node<T>, Boolean> visited;
 
     public Node<T> DepthFirstSearch(Node<T> root, Node<T> goal)
@@ -19,17 +15,18 @@ public class DepthFirstSearch<T>
         if(this.root == this.goal)
         {
             System.out.println("The goal has been reached.");
-            return this.root;
-        }else
-        {
+
+        } else {
             if(root.children.size()-1 >0)
-                child = this.root.children.get(root.children.size()-1);
-
-
-            DepthFirstSearch(child, this.goal);
-            visited.put(child, true);
-            return child;
+            {
+                for(Node<T>child: root.children)
+                {
+                    DepthFirstSearch(child, goal);
+                }
+            }
         }
+
+        return this.root;
     }
 
 }
